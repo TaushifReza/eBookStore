03b5faafda2f79b19763e9507bac437c725282fd
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-02-09 18:18:38 +0545

    Add Increse, Decrese and Remove product from cart page. And make Summry UI

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -27,7 +27,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
             ShoppingCartVm = new()
             {
                 ShoppingCartList =
-                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, 
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
                         includeProperties: "Product")
             };
 
@@ -39,16 +39,63 @@ namespace BulkyWeb.Areas.Customer.Controllers
             return View(ShoppingCartVm);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if (cartFromDb.Count <= 1)
+            {
+                //remove that from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                //decrease by 1
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
         private double GetPriceBaseOnQuantity(ShoppingCart shoppingCart)
         {
-            if (shoppingCart.Count <= 50) {
+            if (shoppingCart.Count <= 50)
+            {
                 return shoppingCart.Product.Price;
             }
-            else {
-                if (shoppingCart.Count <= 100) {
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
                     return shoppingCart.Product.Price50;
                 }
-                else {
+                else
+                {
                     return shoppingCart.Product.Price100;
                 }
             }
