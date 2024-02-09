acc4adcca110ac747ebf17d633699869f49e0d73
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-02-10 03:29:04 +0545

    display order item in admin area

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader>? objOrderHeader = _unitOfWork.OrderHeader
+                .GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeader });
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyWeb/wwwroot/js/order.js
@@ -0,0 +1,31 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/order/getall',
+        },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "30%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'applicationUser.email', "width": "10%" },
+            { data: 'orderStatus', "width": "15%" },
+            { data: 'orderTotal', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/order/details?orderId=${data}"
+                     class="btn btn-primary mx-2">Edit</a>               
+                    </div>`
+                },
+                "width": "15%"
+            },
+        ]
+    });
+}
